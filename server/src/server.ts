f5a9a567a481bb70e9aa86ca23b287c932ad6518
Gilson da Gama <gilsondagama@hotmail.com>
2020-05-13 20:44:00 -0300

    server: lidando com tratativas de erros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,34 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
-import uploadConfig from './config/upload'
+import uploadConfig from './config/upload';
+import Apperror from './errors/AppError';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory))
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof Apperror) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log('Server started on port 3333');
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Errror',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
