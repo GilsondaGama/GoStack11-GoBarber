9ab060c2ae4f05e1a61ef7bcd807e5ec1bea6956
Gilson da Gama <gilsondagama@hotmail.com>
2020-05-23 03:11:09 -0300

    mobile: Configuração da estrutura, Padrões de estilo, Configurando StatusBar, Configuração Navegação

---
+++ mobile/src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return <Container />;
+};
+
+export default SignUp;
