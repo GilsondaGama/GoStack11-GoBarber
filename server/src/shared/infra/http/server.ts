977cfd30f8e6bfdd4024e1e4255a5d0b09372662
Gilson da Gama <gilsondagama@hotmail.com>
2020-05-27 12:12:06 -0300

    mobile: Separação dos módulos, Camada de Infra, Configuração de Imports

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -3,11 +3,12 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import routes from './routes';
-import uploadConfig from './config/upload';
-import Apperror from './errors/AppError';
 
-import './database';
+import uploadConfig from '@config/upload';
+import Apperror from '@shared/errors/AppError';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
 
 const app = express();
 
