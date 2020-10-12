2fcdced69705394e22debd16d44e9594cae4ac70
Gilson da Gama <gilsondagama@hotmail.com>
2020-10-12 08:38:02 -0300

    mobile: Create routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-// import { View } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack'
 
-// import { Container } from './styles';
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
 
-const routes: React.FC = () => {
-  return <View />;
-};
+const Auth = createStackNavigator()
 
-export default routes;
+const AuthRotes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e38'}
+    }}
+  >
+    <Auth.Screen name='SignIn' component={SignIn}/>
+    <Auth.Screen name='SignUP' component={SignUp}/>
+  </Auth.Navigator>
+)
+
+export default AuthRotes;
